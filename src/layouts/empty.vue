da1ecb6b9b5eec4a0700e7e0c531747b62ab5954
ertu426 <erxat124@gmail.com>
2023-06-05 01:41:39 +0800

    feat: ✨ Initial completion of the login page

---
+++ src/layouts/empty.vue
@@ -0,0 +1,12 @@
+<template>
+  <main class="main">
+    <RouterView />
+    <TheFooter />
+  </main>
+</template>
+
+<style scoped lang="scss">
+.main {
+  @apply w-full h-full text-gray-700 dark:text-gray-200 flex flex-col justify-center items-center;
+}
+</style>
